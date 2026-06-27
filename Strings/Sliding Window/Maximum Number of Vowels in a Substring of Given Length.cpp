f67c87da28869353b574a0fd70c649b8a477d406
niranjanjadhav2008-cmd <niranjanjadhav2008@gmail.com>
2026-06-27 20:44:25 +0530

    Improved

--- Strings/Sliding Window/Maximum Number of Vowels in a Substring of Given Length.cpp
+++ Strings/Sliding Window/Maximum Number of Vowels in a Substring of Given Length.cpp
@@ -5,22 +5,21 @@ public:
             return 0;
         }
         int i = 0;
-        int j = 0;
         int vowelCount = 0;
         int maxVowelCount = 0;
-    while(j<s.size()){
+    for(int j = 0;j<s.size();j++){
+        if(s[j]=='a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u'){
+            vowelCount++;
+        }
         if(j-i+1>k){
-            if(s[i]=='a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i]== 'u'){
-                vowelCount--;
+            if(s[i]=='a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'){
+            vowelCount--;
             }
             i++;
         }
-        if(s[j]=='a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j]== 'u'){
-            vowelCount++;
+        if(j-i+1==k){
             maxVowelCount = max(maxVowelCount,vowelCount);
         }
-        j++;
-        
     }
     return maxVowelCount;
     }
